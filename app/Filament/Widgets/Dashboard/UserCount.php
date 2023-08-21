46a41093767397c0c95fffb2372755984bc96094
deimer123 <deimervillamizar@gmail.com>
2023-08-20 20:00:25 -0500

    primer

---
+++ app/Filament/Widgets/Dashboard/UserCount.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Widgets\Dashboard;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use App\Models\Venta;
+use App\Models\User;
+use App\Models\Lote;
+
+class UserCount extends BaseWidget
+{
+
+    protected static ?string $pollingInterval = null;
+
+    protected function getCards(): array
+    {
+        return [
+            Card::make('Numero Clientes ', User::count())
+            ->icon('heroicon-o-user')
+            ->descriptionIcon('heroicon-s-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
+            ->color('success'),
+            Card::make('Numero Lotes ', Lote::count())
+            ->icon('heroicon-o-academic-cap')
+            ->descriptionIcon('heroicon-s-trending-up')
+            ->chart([7, 2, 5, 3, 8, 4, 20])
+            ->color('success'),
+            Card::make('Numero Ventas ', Venta::count())
+            ->icon('heroicon-o-presentation-chart-line')
+            ->descriptionIcon('heroicon-s-trending-up')
+            ->chart([7, 2, 10, 3, 2, 4, 1])
+            ->color('success'),
+        ];
+    }
+}
---
+++ app/Filament/Widgets/Dashboard/UsersChart.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Widgets\Dashboard;
+
+use Filament\Widgets\BarChartWidget;
+use App\Models\User;
+use Carbon\Carbon;
+
+class UsersChart extends BarChartWidget
+{
+    protected static ?string $heading = 'Usuarios';
+
+    protected function getData(): array{
+        $users = User::select('created_at')->get()->groupBy(function($users){
+            return Carbon::parse($users->created_at)->format('F');
+        });
+        $quantities = [];
+        foreach ($users as $user => $value) {
+            array_push($quantities, $value->count());
+        }
+        return[
+            'datasets'=>[
+                [
+                    'label' => 'Usuarios Creados',
+                    'data' => $quantities,
+                    'backgroundColor' => [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    'borderColor'=> [
+                        'rgb(255, 99, 132',
+                        'rgb(255, 159, 64',
+                        'rgb(255, 205, 86',
+                        'rgb(255, 192, 192',
+                        'rgb(255, 162, 235',
+                        'rgb(255, 102, 255',
+                        'rgb(255, 203, 207'
+                    ],
+                    'borderWidth' => 1
+                ],
+            ],
+            'labels' => $users->keys(),
+        ];
+    }
+}
